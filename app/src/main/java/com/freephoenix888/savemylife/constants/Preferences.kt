6289073a69e5c9b79f681fdddf6df96ffbf8f21f
FreePhoenix888 <freephoenix888@gmail.com>
2022-04-13 19:48:55 +0600

    Add preferences

---
+++ app/src/main/java/com/freephoenix888/savemylife/constants/Preferences.kt
@@ -0,0 +1,8 @@
+package com.freephoenix888.savemylife.preferences
+
+object Preferences {
+    const val IS_MAIN_SERVICE_ENABLED = "IS_MAIN_SERVICE_ENABLED"
+    const val IS_LOCATION_SHARING_ENABLED = "IS_LOCATION_SHARING_ENABLED"
+    const val MESSAGE_TEMPLATE = "MESSAGE_TEMPLATE"
+    const val MESSAGE_SENDING_INTERVAL = "MESSAGE_SENDING_INTERVAL"
+}
