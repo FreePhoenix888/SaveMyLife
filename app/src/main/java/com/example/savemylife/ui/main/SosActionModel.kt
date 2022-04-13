28d017bad62d20140f31688f4cd1cc96dce9b6d8
FreePhoenix888 <freephoenix888@gmail.com>
2022-04-13 19:46:07 +0600

    Rename package

--- app/src/main/java/com/example/savemylife/ui/main/SosActionModel.kt
+++
@@ -1,9 +0,0 @@
-package com.example.savemylife.ui.main
-
-import android.app.Activity
-import android.view.View
-import android.widget.ImageView
-
-data class SosActionModel(val iconResourceId: Int, val title: String, val state: Boolean, val settingsButtonOnClickListener: View.OnClickListener) {
-
-}
