045c1b6350eebe97958c81da86c09169940360e8
FreePhoenix888 <freephoenix888@gmail.com>
2022-04-13 19:52:15 +0600

    Add Setting model

---
+++ app/src/main/java/com/freephoenix888/savemylife/data/models/Setting.kt
@@ -0,0 +1,6 @@
+package com.freephoenix888.savemylife.data.models
+
+import android.net.Uri
+import android.view.View
+
+data class Setting(val iconDrawableId: Int, val name: String, val onClickListener: View.OnClickListener)
