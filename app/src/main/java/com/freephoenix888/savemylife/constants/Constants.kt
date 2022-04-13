0a90206a983595736cd8a98c92a58862cafcf424
FreePhoenix888 <freephoenix888@gmail.com>
2022-04-13 19:48:50 +0600

    Add constants

---
+++ app/src/main/java/com/freephoenix888/savemylife/constants/Constants.kt
@@ -0,0 +1,12 @@
+package com.freephoenix888.savemylife.constants
+
+object Constants {
+    const val APP_PACKAGE_NAME = "com.freephoenix888.savemylife"
+    const val PREFERENCES_FILE_PATH = "${APP_PACKAGE_NAME}_PREFERENCES"
+    const val NOTIFICATION_CHANNEL_ID = "tracking_channel"
+    const val NOTIFICATION_CHANNEL_NAME = "Tracking"
+    const val NOTIFICATION_ID = 1
+    const val ACTION_SHOW_EMERGENCY_BUTTON_FRAGMENT = "ACTION_SHOW_EMERGENCY_BUTTON_FRAGMENT"
+    const val ACTION_START_SERVICE = "ACTION_START_SERVICE"
+    const val ACTION_STOP_SERVICE = "ACTION_STOP_SERVICE"
+}
