e5f1ff410e247ca5e325ade38489c38be3f356bd
FreePhoenix888 <freephoenix888@gmail.com>
2022-04-14 15:08:44 +0600

    Move Contact to db/entities

---
+++ app/src/main/java/com/freephoenix888/savemylife/data/db/entities/Contact.kt
@@ -0,0 +1,17 @@
+package com.freephoenix888.savemylife.data.db.entities
+
+import android.net.Uri
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "contact")
+class Contact(
+    @ColumnInfo(name = "phone_number") val phoneNumber: String,
+    @ColumnInfo(name = "name") val name: String,
+    @ColumnInfo(name = "thumbnail_uri") val thumbnailUri: Uri
+) {
+    @PrimaryKey(autoGenerate = true)
+    @ColumnInfo(name = "id")
+    val id: Int? = null
+}
--- app/src/main/java/com/freephoenix888/savemylife/data/models/Contact.kt
+++
@@ -1,9 +0,0 @@
-package com.freephoenix888.savemylife.data.models
-
-import android.net.Uri
-import androidx.room.Entity
-import androidx.room.PrimaryKey
-
-@Entity(tableName = "contact")
-class Contact(@PrimaryKey val id: Int, val phoneNumber: String, val name: String, val thumbnailUri: Uri) {
-}
