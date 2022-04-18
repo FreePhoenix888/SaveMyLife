df1784b78ed632410a2d5ad8e4a707da7466a975
FreePhoenix888 <freephoenix888@gmail.com>
2022-04-18 19:13:34 +0600

    Use flows

--- app/src/main/java/com/freephoenix888/savemylife/data/db/ContactDao.kt
+++ app/src/main/java/com/freephoenix888/savemylife/data/db/ContactDao.kt
@@ -3,20 +3,21 @@ package com.freephoenix888.savemylife.data.db
 import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.freephoenix888.savemylife.data.db.entities.Contact
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface ContactDao {
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
-    suspend fun add(contact: Contact): Int
+    suspend fun insertContacts(vararg contacts: Contact): Int
 
     @Delete
-    suspend fun remove(contact: Contact): Int
+    suspend fun removeContacts(vararg contacts: Contact): Int
 
-    @Query("SELECT * FROM contact WHERE id = :id")
-    suspend fun get(id: Int): Contact
+    @Query("SELECT * FROM contact WHERE id IN (:ids)")
+    suspend fun get(vararg ids: Int): Flow<List<Contact>>
 
     @Query("SELECT * FROM contact ORDER BY id ASC")
-    fun getAll(): LiveData<List<Contact>>
+    fun getAll(): Flow<List<Contact>>
 
 }
