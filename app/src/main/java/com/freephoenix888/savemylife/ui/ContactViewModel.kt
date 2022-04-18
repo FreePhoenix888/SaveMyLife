4abdfc095de177bf89374b6c49d7f3b8bab6a47d
FreePhoenix888 <freephoenix888@gmail.com>
2022-04-18 19:37:52 +0600

    Rename Contact to ContactEntity. Rename add to insert

--- app/src/main/java/com/freephoenix888/savemylife/ui/ContactViewModel.kt
+++ app/src/main/java/com/freephoenix888/savemylife/ui/ContactViewModel.kt
@@ -1,21 +1,21 @@
 package com.freephoenix888.savemylife.ui
 
 import androidx.lifecycle.ViewModel
-import com.freephoenix888.savemylife.data.db.entities.Contact
+import com.freephoenix888.savemylife.data.db.entities.ContactEntity
 import com.freephoenix888.savemylife.data.repositories.ContactRepository
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class ContactViewModel(private val repository: ContactRepository): ViewModel() {
-    fun add(contact: Contact) = CoroutineScope(Dispatchers.Main).launch {
-        repository.add(contact)
+    fun add(contact: ContactEntity) = CoroutineScope(Dispatchers.Main).launch {
+        repository.insert(contact)
     }
 
-    fun remove(contact: Contact) = CoroutineScope(Dispatchers.Main).launch {
+    fun remove(contact: ContactEntity) = CoroutineScope(Dispatchers.Main).launch {
         repository.remove(contact)
     }
 
-    fun getAll(contact: Contact) = repository.getAll()
+    fun getAll(contact: ContactEntity) = repository.getAll()
 
 }
