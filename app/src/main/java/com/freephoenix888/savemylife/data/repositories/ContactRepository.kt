5fc10062e29602809eec6a89516e2620908af9a6
FreePhoenix888 <freephoenix888@gmail.com>
2022-04-18 19:37:26 +0600

    Use flow, get dao by using dao function, use insert function istead of add, use * operator to pass vararg

--- app/src/main/java/com/freephoenix888/savemylife/data/repositories/ContactRepository.kt
+++ app/src/main/java/com/freephoenix888/savemylife/data/repositories/ContactRepository.kt
@@ -1,19 +1,19 @@
 package com.freephoenix888.savemylife.data.repositories
 
-import androidx.lifecycle.LiveData
 import com.freephoenix888.savemylife.data.db.ContactDatabase
-import com.freephoenix888.savemylife.data.db.entities.Contact
+import com.freephoenix888.savemylife.data.db.entities.ContactEntity
+import kotlinx.coroutines.flow.Flow
 
 class ContactRepository(private val db: ContactDatabase) {
 
-    fun getAll(): LiveData<List<Contact>> = db.dao.getAll()
+    fun getAll(): Flow<List<ContactEntity>> = db.dao().getAll()
 
-    suspend fun add(contact: Contact): Int{
-        return db.dao.add(contact)
+    suspend fun insert(vararg contacts: ContactEntity): Int{
+        return db.dao().insert(*contacts)
     }
 
-    suspend fun remove(contact: Contact): Int {
-        return db.dao.remove(contact)
+    suspend fun remove(vararg contacts: ContactEntity): Int {
+        return db.dao().remove(*contacts)
     }
 
 
