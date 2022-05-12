01e9438bd5260c7255b74dd92004af4ce6d7b298
FreePhoenix888 <freephoenix888@gmail.com>
2022-05-12 15:55:19 +0600

    Use Dagger

--- app/src/main/java/com/freephoenix888/savemylife/data/repositories/ContactRepository.kt
+++ app/src/main/java/com/freephoenix888/savemylife/data/repositories/ContactRepository.kt
@@ -1,23 +1,23 @@
 package com.freephoenix888.savemylife.data.repositories
 
-import com.freephoenix888.savemylife.data.db.ContactDatabase
+import com.freephoenix888.savemylife.data.db.ContactLocalStorage
 import com.freephoenix888.savemylife.data.db.entities.ContactEntity
 import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.StateFlow
+import javax.inject.Inject
+import javax.inject.Singleton
 
-class ContactRepository(private val db: ContactDatabase) {
+@Singleton
+class ContactRepository @Inject constructor (val localStorage: ContactLocalStorage) {
 
-    suspend fun insert(vararg contacts: ContactEntity): Int{
-        return db.dao().insert(*contacts)
+    suspend fun insert(vararg contacts: ContactEntity){
+        localStorage.dao().insert(*contacts)
     }
 
-    suspend fun remove(vararg contacts: ContactEntity): Int {
-        return db.dao().remove(*contacts)
+    suspend fun remove(vararg contacts: ContactEntity) {
+        localStorage.dao().remove(*contacts)
     }
 
-    suspend fun get(vararg contactIds: Int): Flow<List<ContactEntity>>{
-        return db.dao().get(*contactIds)
-    }
-
-    fun getAll(): Flow<List<ContactEntity>> = db.dao().getAll()
+    fun getAll(): Flow<List<ContactEntity>> = localStorage.dao().getAll()
 
 }
