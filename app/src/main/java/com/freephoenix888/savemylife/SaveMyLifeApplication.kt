0dc174b1b23e21b01f231dfb61854ab3b08a6adb
FreePhoenix888 <freephoenix888@gmail.com>
2022-05-12 15:57:16 +0600

    Use dagger

--- app/src/main/java/com/freephoenix888/savemylife/SaveMyLifeApplication.kt
+++ app/src/main/java/com/freephoenix888/savemylife/SaveMyLifeApplication.kt
@@ -1,11 +1,23 @@
 package com.freephoenix888.savemylife
 
 import android.app.Application
-import com.freephoenix888.savemylife.data.db.ContactDatabase
 import com.freephoenix888.savemylife.data.repositories.ContactRepository
-import com.freephoenix888.savemylife.ui.ContactViewModelFactory
+import com.freephoenix888.savemylife.di.SaveMyLifeModule
+import com.freephoenix888.savemylife.ui.ContactViewModel
+import dagger.Component
+import javax.inject.Singleton
+
+@Singleton
+@Component(modules = [SaveMyLifeModule::class])
+interface ApplicationComponent {
+    fun inject(activity: SaveMyLifeActivity)
+    fun inject(repository: ContactRepository)
+    fun inject(viewModel: ContactViewModel)
+}
 
 
 class SaveMyLifeApplication() : Application() {
-
+    val appComponent = DaggerApplicationComponent.builder()
+        .saveMyLifeModule(SaveMyLifeModule(application = this))
+        .build()
 }
