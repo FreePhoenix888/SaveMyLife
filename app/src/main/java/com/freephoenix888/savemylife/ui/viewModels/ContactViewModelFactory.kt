fc5472f086425e40346a49e641c5b263cffdb985
FreePhoenix888 <freephoenix888@gmail.com>
2022-05-16 20:46:31 +0600

    Add imports

--- app/src/main/java/com/freephoenix888/savemylife/ui/viewModels/ContactViewModelFactory.kt
+++ app/src/main/java/com/freephoenix888/savemylife/ui/viewModels/ContactViewModelFactory.kt
@@ -3,6 +3,10 @@ package com.freephoenix888.savemylife.ui.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.freephoenix888.savemylife.data.repositories.ContactRepository
+import com.freephoenix888.savemylife.domain.useCases.DeleteContactsUseCase
+import com.freephoenix888.savemylife.domain.useCases.IDeleteContactsUseCase
+import com.freephoenix888.savemylife.domain.useCases.ISaveContactsUseCase
+import com.freephoenix888.savemylife.domain.useCases.SaveContactsUseCase
 
 class ContactViewModelFactory(
     private val repository: ContactRepository
