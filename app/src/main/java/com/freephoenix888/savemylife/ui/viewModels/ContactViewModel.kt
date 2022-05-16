e39092bfa0700f3d1c3a43b8064148330b6fc7d5
FreePhoenix888 <freephoenix888@gmail.com>
2022-05-16 20:46:24 +0600

    Remove imports

--- app/src/main/java/com/freephoenix888/savemylife/ui/viewModels/ContactViewModel.kt
+++ app/src/main/java/com/freephoenix888/savemylife/ui/viewModels/ContactViewModel.kt
@@ -3,10 +3,8 @@ package com.freephoenix888.savemylife.ui.viewModels
 import androidx.lifecycle.*
 import com.freephoenix888.savemylife.data.storage.entities.ContactEntity
 import com.freephoenix888.savemylife.data.repositories.ContactRepository
-import com.freephoenix888.savemylife.domain.useCases.DeleteContactsUseCase
 import com.freephoenix888.savemylife.domain.useCases.IDeleteContactsUseCase
 import com.freephoenix888.savemylife.domain.useCases.ISaveContactsUseCase
-import com.freephoenix888.savemylife.domain.useCases.SaveContactsUseCase
 import javax.inject.Inject
 
 class ContactViewModel @Inject constructor (private val repository: ContactRepository, private val saveContactsUseCase: ISaveContactsUseCase, private val deleteContactsUseCase: IDeleteContactsUseCase): ViewModel() {
