abdc780ce3a8bd975d882c9eac7f1d15a6de3e63
FreePhoenix888 <freephoenix888@gmail.com>
2022-05-16 13:04:32 +0600

    Add room and kapt plugin

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,12 @@
+import org.jetbrains.kotlin.base.kapt3.KaptOptions
+import org.jetbrains.kotlin.kapt3.base.Kapt.kapt
 import kotlin.reflect.full.memberProperties
 
 plugins {
     id("com.android.application")
     id("kotlin-android")
     id("androidx.navigation.safeargs")
+    id("kotlin-kapt")
 }
 
 android {
@@ -47,7 +50,16 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.4.1")
     implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.4.1")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.4.1")
-    implementation("androidx.room:room-ktx:2.4.2")
+    implementation("androidx.legacy:legacy-support-v4:1.0.0")
+    val roomVersion = "2.4.2"
+    implementation("androidx.room:room-runtime:$roomVersion")
+    annotationProcessor("androidx.room:room-compiler:$roomVersion")
+    // To use Kotlin annotation processing tool (kapt)
+    kapt("androidx.room:room-compiler:$roomVersion")
+    // optional - Kotlin Extensions and Coroutines support for Room
+    implementation("androidx.room:room-ktx:$roomVersion")
     implementation("commons-io:commons-io:2.5")
     implementation("pub.devrel:easypermissions:3.0.0")
+    implementation("com.google.dagger:dagger:2.28.3")
+    kapt("com.google.dagger:dagger-compiler:2.28.3")
 }
