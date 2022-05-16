ce56567508beedf40e31f9d441c336dd3f2578cd
FreePhoenix888 <freephoenix888@gmail.com>
2022-05-16 13:05:14 +0600

    Remove all fields but uri and make uri primary key

--- app/src/main/java/com/freephoenix888/savemylife/data/db/entities/ContactEntity.kt
+++ app/src/main/java/com/freephoenix888/savemylife/data/db/entities/ContactEntity.kt
@@ -3,14 +3,12 @@ package com.freephoenix888.savemylife.data.db.entities
 import android.net.Uri
 import androidx.room.ColumnInfo
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "contact")
 data class ContactEntity(
-    @PrimaryKey(autoGenerate = true) @ColumnInfo(name = "id") val id: Int? = null,
-    @ColumnInfo(name = "phone_number") val phoneNumber: String,
-    @ColumnInfo(name = "name") val name: String,
-    @ColumnInfo(name = "thumbnail_uri") val thumbnailUri: String
+    @PrimaryKey @ColumnInfo(name = "uri") val uri: String,
 ) {
 
 }
