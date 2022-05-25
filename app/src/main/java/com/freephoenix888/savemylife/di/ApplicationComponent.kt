bb2f306894923b738985d3e3a323038770024a6d
FreePhoenix888 <freephoenix888@gmail.com>
2022-05-25 20:49:00 +0600

    Update di files

--- app/src/main/java/com/freephoenix888/savemylife/di/ApplicationComponent.kt
+++ app/src/main/java/com/freephoenix888/savemylife/di/ApplicationComponent.kt
@@ -1,15 +1,12 @@
 package com.freephoenix888.savemylife.di
 
 import com.freephoenix888.savemylife.data.repositories.ContactRepository
-import com.freephoenix888.savemylife.data.storage.local.ContactLocalStorage
-import com.freephoenix888.savemylife.di.SaveMyLifeModule
-import com.freephoenix888.savemylife.domain.useCases.DeleteContactsUseCase
-import com.freephoenix888.savemylife.domain.useCases.IDeleteContactsUseCase
-import com.freephoenix888.savemylife.domain.useCases.ISaveContactsUseCase
+import com.freephoenix888.savemylife.data.room.databases.ContactDatabase
+import com.freephoenix888.savemylife.domain.useCases.interfaces.DeleteContactsUseCase
+import com.freephoenix888.savemylife.domain.useCases.interfaces.SaveContactsUseCase
 import com.freephoenix888.savemylife.services.MainService
 import com.freephoenix888.savemylife.ui.SaveMyLifeActivity
-import com.freephoenix888.savemylife.ui.fragments.ContactsSettingsFragment
-import com.freephoenix888.savemylife.ui.viewModels.ContactViewModel
+import com.freephoenix888.savemylife.ui.viewModels.ContactsViewModel
 import dagger.Component
 import javax.inject.Singleton
 
@@ -19,10 +16,9 @@ interface ApplicationComponent {
     fun mainServiceComponent(): MainServiceComponent.Factory
     fun inject(activity: SaveMyLifeActivity)
     fun inject(repository: ContactRepository)
-    fun inject(viewModel: ContactViewModel)
-    fun inject(contactLocalStorage: ContactLocalStorage)
-    fun inject(contactsSettingsFragment: ContactsSettingsFragment)
-    fun inject(saveContactsUseCase: ISaveContactsUseCase)
-    fun inject(deleteContactsUseCase: IDeleteContactsUseCase)
+    fun inject(viewModel: ContactsViewModel)
+    fun inject(contactDatabase: ContactDatabase)
+    fun inject(saveContactsUseCase: SaveContactsUseCase)
+    fun inject(deleteContactsUseCase: DeleteContactsUseCase)
     fun inject(mainService: MainService)
 }
--- app/src/main/java/com/freephoenix888/savemylife/di/SaveMyLifeModule.kt
+++ app/src/main/java/com/freephoenix888/savemylife/di/SaveMyLifeModule.kt
@@ -1,52 +1,86 @@
 package com.freephoenix888.savemylife.di
 
 import android.app.Application
+import android.content.Context
 import androidx.room.Room
-import com.freephoenix888.savemylife.data.storage.local.ContactLocalStorage
 import com.freephoenix888.savemylife.data.repositories.ContactRepository
-import com.freephoenix888.savemylife.domain.useCases.DeleteContactsUseCase
-import com.freephoenix888.savemylife.domain.useCases.IDeleteContactsUseCase
-import com.freephoenix888.savemylife.domain.useCases.ISaveContactsUseCase
-import com.freephoenix888.savemylife.domain.useCases.SaveContactsUseCase
-import com.freephoenix888.savemylife.ui.viewModels.ContactViewModelFactory
+import com.freephoenix888.savemylife.data.room.databases.ContactDatabase
+import com.freephoenix888.savemylife.data.sources.ContactsLocalDataSource
+import com.freephoenix888.savemylife.domain.useCases.DeleteLocalContactsUseCase
+import com.freephoenix888.savemylife.domain.useCases.GetLocalDangerModeStateUseCase
+import com.freephoenix888.savemylife.domain.useCases.SaveLocalContactsUseCase
+import com.freephoenix888.savemylife.domain.useCases.interfaces.DeleteContactsUseCase
+import com.freephoenix888.savemylife.domain.useCases.interfaces.GetContactByUriUseCase
+import com.freephoenix888.savemylife.domain.useCases.interfaces.GetDangerModeStateUseCase
+import com.freephoenix888.savemylife.domain.useCases.interfaces.SaveContactsUseCase
+import com.freephoenix888.savemylife.ui.SaveMyLifeActivity
+import com.freephoenix888.savemylife.ui.viewModels.ContactsViewModelFactory
 import dagger.Module
 import dagger.Provides
+import dagger.hilt.InstallIn
 import javax.inject.Singleton
 
 @Module
-class SaveMyLifeModule(val application: Application) {
+@InstallIn(SaveMyLifeActivity::class)
+abstract class SaveMyLifeModule(val application: Application) {
 
     @Singleton
     @Provides
-    fun provideContactLocalStorage(): ContactLocalStorage {
+    fun provideContactLocalStorage(): ContactDatabase {
         return Room.databaseBuilder(
             application,
-            ContactLocalStorage::class.java,
+            ContactDatabase::class.java,
             "Contact.db"
         ).build()
     }
 
     @Singleton
     @Provides
-    fun provideContactViewModel(repository: ContactRepository, saveContactsUseCase: ISaveContactsUseCase, deleteContactsUseCase: IDeleteContactsUseCase) = ContactViewModelFactory(repository = repository, saveContactsUseCase = saveContactsUseCase, deleteContactsUseCase = deleteContactsUseCase)
+    fun provideContactViewModel(
+        repository: ContactRepository,
+        saveContactsUseCase: SaveContactsUseCase,
+        deleteContactsUseCase: DeleteContactsUseCase,
+        getContactByUriUseCase: GetContactByUriUseCase
+    ) = ContactsViewModelFactory(
+        repository = repository,
+        saveContactsUseCase = saveContactsUseCase,
+        deleteContactsUseCase = deleteContactsUseCase,
+        getContactByUriUseCase = getContactByUriUseCase
+    )
 
     @Singleton
     @Provides
-    fun provideYourDao(localStorage: ContactLocalStorage) = localStorage.dao()
+    fun provideYourDao(localStorage: ContactDatabase) = localStorage.dao()
 
     @Singleton
     @Provides
-    fun provideContactRepository(contactLocalStorage: ContactLocalStorage) = ContactRepository(contactLocalStorage)
+    fun provideContactRepository(contactDatabase: ContactDatabase) =
+        ContactRepository(ContactsLocalDataSource(database = contactDatabase))
 
     @Singleton
     @Provides
-    fun provideSaveContactsUseCase(): ISaveContactsUseCase {
-        return SaveContactsUseCase()
+    fun provideSaveContactsUseCase(repository: ContactRepository): SaveContactsUseCase {
+        return SaveLocalContactsUseCase()
     }
 
     @Singleton
     @Provides
-    fun provideDeleteContactsUseCase(): IDeleteContactsUseCase {
-        return DeleteContactsUseCase()
+    fun provideDeleteContactsUseCase(): DeleteContactsUseCase {
+        return DeleteLocalContactsUseCase()
+    }
+
+    @Singleton
+    @Provides
+    fun provideGetIsDangerModeEnabledUseCase(): GetDangerModeStateUseCase {
+        return GetLocalDangerModeStateUseCase()
+    }
+
+    @Singleton
+    @Provides
+    fun provideIsDangerModeEnabledInitialValue(
+        context: Context,
+        getDangerModeStateUseCase: GetDangerModeStateUseCase
+    ): Boolean {
+        return getDangerModeStateUseCase(context)
     }
 }
