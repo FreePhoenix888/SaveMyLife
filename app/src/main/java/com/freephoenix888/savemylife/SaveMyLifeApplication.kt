a432b98ef15ffe512e93f4967c4c9280ceb343d4
FreePhoenix888 <freephoenix888@gmail.com>
2022-05-25 20:45:49 +0600

    Use hilt

--- app/src/main/java/com/freephoenix888/savemylife/SaveMyLifeApplication.kt
+++ app/src/main/java/com/freephoenix888/savemylife/SaveMyLifeApplication.kt
@@ -3,10 +3,11 @@ package com.freephoenix888.savemylife
 import android.app.Application
 import com.freephoenix888.savemylife.di.DaggerApplicationComponent
 import com.freephoenix888.savemylife.di.SaveMyLifeModule
+import dagger.hilt.android.HiltAndroidApp
 
-
+@HiltAndroidApp
 class SaveMyLifeApplication() : Application() {
-    val appComponent = DaggerApplicationComponent.builder()
-        .saveMyLifeModule(SaveMyLifeModule(application = this))
-        .build()
+//    val appComponent = DaggerApplicationComponent.builder()
+//        .saveMyLifeModule(SaveMyLifeModule(application = this))
+//        .build()
 }
