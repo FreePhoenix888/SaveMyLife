f9f35cad5c865737ade9256d849a4229c8f31c44
FreePhoenix888 <freephoenix888@gmail.com>
2022-05-25 20:47:11 +0600

    Update Activity

--- app/src/main/java/com/freephoenix888/savemylife/ui/SaveMyLifeActivity.kt
+++ app/src/main/java/com/freephoenix888/savemylife/ui/SaveMyLifeActivity.kt
@@ -2,46 +2,30 @@ package com.freephoenix888.savemylife.ui
 
 import android.content.Intent
 import android.os.Bundle
+import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.NavController
-import androidx.navigation.fragment.NavHostFragment
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.setupWithNavController
-import com.freephoenix888.savemylife.NavGraphDirections
-import com.freephoenix888.savemylife.R
-import com.freephoenix888.savemylife.SaveMyLifeApplication
 import com.freephoenix888.savemylife.constants.ActionConstants
-import com.freephoenix888.savemylife.databinding.ActivitySaveMyLifeBinding
 import com.freephoenix888.savemylife.services.MainService
-import com.freephoenix888.savemylife.ui.viewModels.ContactViewModel
-import javax.inject.Inject
+import com.freephoenix888.savemylife.ui.composables.SaveMyLifeApp
+import dagger.hilt.android.AndroidEntryPoint
 
-
+@AndroidEntryPoint
 class SaveMyLifeActivity : AppCompatActivity()  {
 
-    @Inject lateinit var viewModel: ContactViewModel
-    private lateinit var binding: ActivitySaveMyLifeBinding
-    private lateinit var navController: NavController
-
     override fun onCreate(savedInstanceState: Bundle?) {
-        (applicationContext as SaveMyLifeApplication).appComponent.inject(this)
         super.onCreate(savedInstanceState)
-        binding = ActivitySaveMyLifeBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
-        navController = navHostFragment.navController
-        navigateToEmergencyButtonFragmentIfNeeded(intent)
-        setupToolbar()
-        runMainService()
+        setContent {
+            SaveMyLifeApp(intent = intent)
+        }
     }
 
-    private fun setupToolbar(){
-        val toolbar = binding.toolbarMain.toolbar
-        val toolBarConfig = AppBarConfiguration(navController.graph)
-        setSupportActionBar(toolbar)
-        toolbar.setupWithNavController(navController, toolBarConfig)
-        supportActionBar?.setDisplayShowTitleEnabled(false)
-    }
+//    private fun setupToolbar(){
+//        val toolbar = binding.toolbarMain.toolbar
+//        val toolBarConfig = AppBarConfiguration(navController.graph)
+//        setSupportActionBar(toolbar)
+//        toolbar.setupWithNavController(navController, toolBarConfig)
+//        supportActionBar?.setDisplayShowTitleEnabled(false)
+//    }
 
     override fun onNewIntent(intent: Intent?) {
         super.onNewIntent(intent)
@@ -50,16 +34,17 @@ class SaveMyLifeActivity : AppCompatActivity()  {
 
     private fun runMainService(){
         Intent(this, MainService::class.java).also {
-            it.action = ActionConstants.START_SERVICE
+            it.action = ActionConstants.StartMainService
             startService(it)
         }
     }
 
-    private fun navigateToEmergencyButtonFragmentIfNeeded(intent: Intent?){
-        if(intent?.action == ActionConstants.SHOW_EMERGENCY_BUTTON_FRAGMENT){
-            val emergencyButtonDirection = NavGraphDirections.actionGlobalEmergencyButton()
-            navController.navigate(emergencyButtonDirection)
-        }
+    private fun processIntentAction(intent: Intent?){
+        navigateToEmergencyButtonFragmentIfNeeded(intent)
+
     }
 
+    private fun navigateToEmergencyButtonFragmentIfNeeded(intent: Intent?){
+
+    }
 }
