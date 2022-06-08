2888bd83bcd988e063ff2fb4173d6e4ca08a888f
FreePhoenix888 <freephoenix888@gmail.com>
2022-06-08 12:12:12 +0600

    Add EmergencyMessageConstants

---
+++ app/src/main/java/com/freephoenix888/savemylife/constants/EmergencyMessageConstants.kt
@@ -0,0 +1,8 @@
+package com.freephoenix888.savemylife.constants
+
+import com.freephoenix888.savemylife.SecondsInterval
+
+object EmergencyMessageConstants {
+    const val DEFAULT_EMERGENCY_MESSAGE_SENDING_SECONDS_INTERVAL: SecondsInterval = 30
+    const val DEFAULT_EMERGENCY_MESSAGE_TEMPLATE: String = "{CONTACT_NAME}, I AM IN DANGER! My location: {LOCATION_URL}"
+}
