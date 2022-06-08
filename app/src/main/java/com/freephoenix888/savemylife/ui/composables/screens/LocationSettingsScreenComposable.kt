439be9493eb57cdf0b990e2cfc021a32761a55ff
FreePhoenix888 <freephoenix888@gmail.com>
2022-06-08 19:43:50 +0600

    Rename

--- app/src/main/java/com/freephoenix888/savemylife/ui/composables/screens/LocationSettingsScreenComposable.kt
+++ app/src/main/java/com/freephoenix888/savemylife/ui/composables/screens/LocationSettingsScreenComposable.kt
@@ -1,18 +1,21 @@
 package com.freephoenix888.savemylife.ui.composables
 
-import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.selection.toggleable
-import androidx.compose.material.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Icon
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ShareLocation
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.alorma.compose.settings.storage.base.rememberBooleanSettingState
+import com.alorma.compose.settings.ui.SettingsSwitch
 import com.freephoenix888.savemylife.ui.viewModels.LocationViewModel
 
 @Composable
@@ -21,8 +24,8 @@ fun LocationSettingsScreenComposable(
 ) {
     val locationSharingState by locationViewModel.locationSharingState.collectAsState(initial = false)
     LocationSettingsScreenBodyComposable(
-        locationSharingState = locationSharingState,
-        onLocationSharingStateChanged = { state: Boolean ->
+        isLocationSharingEnabled = locationSharingState,
+        onChangeLocationSharingState = { state: Boolean ->
             locationViewModel.setLocationSharingState(state)
         }
     )
@@ -30,9 +33,10 @@ fun LocationSettingsScreenComposable(
 
 @Composable
 private fun LocationSettingsScreenBodyComposable(
-    locationSharingState: Boolean,
-    onLocationSharingStateChanged: (Boolean) -> Unit
+    isLocationSharingEnabled: Boolean,
+    onChangeLocationSharingState: (Boolean) -> Unit
 ) {
+    val isLocationSharingEnabledState = rememberBooleanSettingState(isLocationSharingEnabled)
     Scaffold() { innerPadding: PaddingValues ->
         Column(
             modifier = Modifier
@@ -40,36 +44,46 @@ private fun LocationSettingsScreenBodyComposable(
                 .fillMaxWidth(),
             horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            Card(
-                border = BorderStroke(
-                    width = 1.dp,
-                    color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
-                ),
-                modifier = Modifier.padding(16.dp)
-            ) {
-                Surface {
-                    Row(
-                        modifier = Modifier
-                            .fillMaxWidth()
-                            .toggleable(
-                                value = locationSharingState,
-                                role = Role.Switch,
-                                onValueChange = { onLocationSharingStateChanged(it) }
-                            ),
-                        verticalAlignment = Alignment.CenterVertically,
-                    ) {
-                        Icon(
-                            imageVector = Icons.Filled.ShareLocation,
-                            contentDescription = "Location sharing"
-                        )
-                        Text(text = "Location sharing")
-                        Switch(
-                            checked = locationSharingState,
-                            onCheckedChange = onLocationSharingStateChanged
-                        )
-                    }
-                }
-            }
+            SettingsSwitch(title = {
+                Text("Location sharing")
+            },
+            icon = {
+                Icon(imageVector = Icons.Filled.ShareLocation, contentDescription = "Location sharing")
+            },
+                state = isLocationSharingEnabledState,
+            onCheckedChange = {
+                onChangeLocationSharingState(!it)
+            })
+        //            Card(
+//                border = BorderStroke(
+//                    width = 1.dp,
+//                    color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
+//                ),
+//                modifier = Modifier.padding(16.dp)
+//            ) {
+//                Surface {
+//                    Row(
+//                        modifier = Modifier
+//                            .fillMaxWidth()
+//                            .toggleable(
+//                                value = locationSharingState,
+//                                role = Role.Switch,
+//                                onValueChange = { onLocationSharingStateChanged(it) }
+//                            ),
+//                        verticalAlignment = Alignment.CenterVertically,
+//                    ) {
+//                        Icon(
+//                            imageVector = Icons.Filled.ShareLocation,
+//                            contentDescription = "Location sharing"
+//                        )
+//                        Text(text = "Location sharing")
+//                        Switch(
+//                            checked = locationSharingState,
+//                            onCheckedChange = onLocationSharingStateChanged
+//                        )
+//                    }
+//                }
+//            }
         }
     }
 }
@@ -79,8 +93,8 @@ private fun LocationSettingsScreenBodyComposable(
 private fun LocationSettingsScreenBodyComposablePreview() {
     var locationSharingState by remember { mutableStateOf(false) }
     LocationSettingsScreenBodyComposable(
-        locationSharingState = locationSharingState,
-        onLocationSharingStateChanged = { state: Boolean ->
+        isLocationSharingEnabled = locationSharingState,
+        onChangeLocationSharingState = { state: Boolean ->
             locationSharingState = state
         }
     )
