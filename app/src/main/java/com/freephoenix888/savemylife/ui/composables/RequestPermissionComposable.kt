3c6281c4d058b10df5d201ad89ead3014b87b654
FreePhoenix888 <freephoenix888@gmail.com>
2022-06-09 16:51:27 +0600

    Just ask permission

--- app/src/main/java/com/freephoenix888/savemylife/ui/composables/RequestPermissionComposable.kt
+++ app/src/main/java/com/freephoenix888/savemylife/ui/composables/RequestPermissionComposable.kt
@@ -9,10 +9,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.freephoenix888.savemylife.Utils
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.PermissionState
-import com.google.accompanist.permissions.shouldShowRationale
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
@@ -30,16 +28,11 @@ fun RequestPermissionComposable(
                 .padding(32.dp)
         ) {
             Text(text)
-            val onGrantPermissionsButtonClick: () -> Unit =
-                if (permissionState.status.shouldShowRationale) {
-                    { permissionState.launchPermissionRequest() }
-                } else {
-                    {
-                        Utils.openAppSettings(context)
-                    }
-                }
+
             Spacer(modifier = Modifier.height(16.dp))
-            Button(onClick = onGrantPermissionsButtonClick) {
+            Button(onClick = {
+                permissionState.launchPermissionRequest()
+            }) {
                 Text("Grant permissions")
             }
         }
