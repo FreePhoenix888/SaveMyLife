c9dfcd21bb9b86c28a250567d9ca8bf311b40e20
FreePhoenix888 <freephoenix888@gmail.com>
2022-06-12 16:22:23 +0600

    Update constants

--- app/src/main/java/com/freephoenix888/savemylife/constants/ActionConstants.kt
+++ app/src/main/java/com/freephoenix888/savemylife/constants/ActionConstants.kt
@@ -1,8 +1,6 @@
 package com.freephoenix888.savemylife.constants
 
 object ActionConstants {
-    const val ShowEmergencyButtonScreen = "ACTION_SHOW_EMERGENCY_BUTTON_FRAGMENT"
     const val StartMainService = "ACTION_START_SERVICE"
     const val StopMainService = "ACTION_STOP_SERVICE"
-    const val SwitchDangerMode = "SWITCH_DANGER_MODE"
 }
