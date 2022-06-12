f962a9732d980d7c7a7b257faa1df1ae915fe4d9
FreePhoenix888 <freephoenix888@gmail.com>
2022-06-12 16:34:41 +0600

    Update PowerButtonBroadcastReceiver.kt

--- app/src/main/java/com/freephoenix888/savemylife/broadcastReceivers/PowerButtonBroadcastReceiver.kt
+++ app/src/main/java/com/freephoenix888/savemylife/broadcastReceivers/PowerButtonBroadcastReceiver.kt
@@ -4,37 +4,43 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
-import com.freephoenix888.savemylife.constants.ActionConstants
-import com.freephoenix888.savemylife.services.MainService
+import com.freephoenix888.savemylife.data.repositories.SaveMyLifeRepository
+import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.runBlocking
 import java.util.*
 import javax.inject.Inject
 import javax.inject.Singleton
 import kotlin.concurrent.schedule
 
 @Singleton
-class PowerButtonBroadcastReceiver @Inject constructor(): BroadcastReceiver() {
+class PowerButtonBroadcastReceiver @Inject constructor(@ApplicationContext private val applicationContext: Context, private val saveMyLifeRepository: SaveMyLifeRepository): BroadcastReceiver() {
 
     val TAG = this::class.java.simpleName
 
     private var _count = 0
-    private val timer = Timer().schedule(5000) {
+    private val _timer = Timer().schedule(5000) {
         Log.d(TAG, "Timer: _count = 0")
         _count = 0
     }
 
     override fun onReceive(context: Context?, intent: Intent?) {
-        val intentAction = intent?.action
+        val intentAction = intent?.action ?: return
         if(intentAction == Intent.ACTION_SCREEN_OFF || intentAction == Intent.ACTION_SCREEN_ON){
             if(_count == 0) {
-                timer.run()
+                _timer.run()
             }
             if(_count == 5) {
-                Intent(context, MainService::class.java).also {
-                    it.action = ActionConstants.SwitchDangerMode
-                    context?.startService(it)
+                runBlocking {
+                    saveMyLifeRepository.setIsDangerModeEnabled(true)
                 }
+//                Intent(context, MainService::class.java).also {
+//                    it.action = ActionConstants.SwitchDangerMode
+//                    context?.startService(it)
+//                }
+
+            } else {
+                ++_count
             }
-            ++_count
             Log.d(TAG, "Power button is clicked. _count: $_count")
         }
     }
