ebf4ec9ef7fad1e9fd3ef90615662dbcdaca445c
FreePhoenix888 <freephoenix888@gmail.com>
2022-06-12 16:19:34 +0600

    Update constants

--- app/src/main/java/com/freephoenix888/savemylife/constants/MessageConstants.kt
+++ app/src/main/java/com/freephoenix888/savemylife/constants/MessageConstants.kt
@@ -2,7 +2,7 @@ package com.freephoenix888.savemylife.constants
 
 import com.freephoenix888.savemylife.SecondsInterval
 
-object EmergencyMessageConstants {
+object MessageConstants {
     const val DEFAULT_EMERGENCY_MESSAGE_SENDING_SECONDS_INTERVAL: SecondsInterval = 30
     const val DEFAULT_EMERGENCY_MESSAGE_TEMPLATE: String = "{CONTACT_NAME}, I AM IN DANGER! My location: {LOCATION_URL}"
 }
---
+++ app/src/main/java/com/freephoenix888/savemylife/constants/PhoneNumberConstants.kt
@@ -0,0 +1,35 @@
+package com.freephoenix888.savemylife.constants
+
+import androidx.core.net.toUri
+import com.freephoenix888.savemylife.domain.models.PhoneNumber
+
+object PhoneNumberConstants {
+
+    val FAKE_PHONE_NUMBERS =
+        listOf(
+            PhoneNumber(
+                phoneNumber = "+7 777 777 7779",
+                contactName = "Name",
+                contactPhotoThumbnailUri = null,
+                contentUri = "0".toUri()
+            ),
+            PhoneNumber(
+                phoneNumber = "+7 777 777 7778",
+                contactName = "Name",
+                contactPhotoThumbnailUri = null,
+                contentUri = "0".toUri()
+            ),
+            PhoneNumber(
+                phoneNumber = "+7 777 777 7797",
+                contactName = "Name",
+                contactPhotoThumbnailUri = null,
+                contentUri = "1".toUri()
+            ),
+            PhoneNumber(
+                phoneNumber = "+7 777 777 7787",
+                contactName = "Name",
+                contactPhotoThumbnailUri = null,
+                contentUri = "1".toUri()
+            ),
+        )
+}
