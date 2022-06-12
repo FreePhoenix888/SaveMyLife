9e0a6dbd14a664b37ecef39486ecdf7abf82ef10
FreePhoenix888 <freephoenix888@gmail.com>
2022-06-12 16:16:37 +0600

    Update data store serializer

--- app/src/main/java/com/freephoenix888/savemylife/data/datastore/EmergencyMessagePreferencesSerializer.kt
+++ app/src/main/java/com/freephoenix888/savemylife/data/datastore/EmergencyMessagePreferencesSerializer.kt
@@ -1,22 +1,21 @@
 package com.freephoenix888.savemylife.data.datastore
 
 import androidx.datastore.core.Serializer
-import com.freephoenix888.savemylife.EmergencyMessagePreferences
 import com.google.protobuf.InvalidProtocolBufferException
 import java.io.InputStream
 import java.io.OutputStream
 import kotlin.coroutines.cancellation.CancellationException
 
-object EmergencyMessagePreferencesSerializer: Serializer<EmergencyMessagePreferences> {
-    override val defaultValue: EmergencyMessagePreferences = EmergencyMessagePreferences.getDefaultInstance()
+object MessagePreferencesSerializer: Serializer<MessagePreferences> {
+    override val defaultValue: MessagePreferences = MessagePreferences.getDefaultInstance()
 
-    override suspend fun readFrom(input: InputStream): EmergencyMessagePreferences {
+    override suspend fun readFrom(input: InputStream): MessagePreferences {
         try {
-            return EmergencyMessagePreferences.parseFrom(input)
+            return MessagePreferences.parseFrom(input)
         } catch (e: InvalidProtocolBufferException) {
             throw CancellationException("Cannot reada proto.", e)
         }
     }
 
-    override suspend fun writeTo(t: EmergencyMessagePreferences, output: OutputStream) = t.writeTo(output)
+    override suspend fun writeTo(t: MessagePreferences, output: OutputStream) = t.writeTo(output)
 }
