b1691b911f6a066f53e3bcc3c4b84568833f9443
FreePhoenix888 <freephoenix888@gmail.com>
2022-06-27 11:30:47 +0600

    Run periodic worker

--- app/src/main/java/com/freephoenix888/savemylife/broadcastReceivers/PowerButtonBroadcastReceiver.kt
+++ app/src/main/java/com/freephoenix888/savemylife/broadcastReceivers/PowerButtonBroadcastReceiver.kt
@@ -4,16 +4,34 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.util.Log
-import com.freephoenix888.savemylife.data.repositories.SaveMyLifeRepository
+import androidx.work.PeriodicWorkRequestBuilder
+import androidx.work.WorkManager
+import com.freephoenix888.savemylife.domain.useCases.GetMessageSendingIntervalUseCase
+import com.freephoenix888.savemylife.workers.DoInDangerWorker
+import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.qualifiers.ApplicationContext
-import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
 import java.util.*
 import javax.inject.Inject
-import javax.inject.Singleton
 import kotlin.concurrent.schedule
+import kotlin.time.toJavaDuration
 
-@Singleton
-class PowerButtonBroadcastReceiver @Inject constructor(@ApplicationContext private val applicationContext: Context, private val saveMyLifeRepository: SaveMyLifeRepository): BroadcastReceiver() {
+@AndroidEntryPoint
+class PowerButtonBroadcastReceiver :
+    BroadcastReceiver() {
+
+    @Inject
+    @ApplicationContext
+    lateinit var applicationContext: Context
+
+    @Inject
+    lateinit var getMessageSendingIntervalFlowUseCase: GetMessageSendingIntervalUseCase
+
+    private val scope = CoroutineScope(Dispatchers.IO + SupervisorJob())
 
     val TAG = this::class.java.simpleName
 
@@ -25,14 +43,22 @@ class PowerButtonBroadcastReceiver @Inject constructor(@ApplicationContext priva
 
     override fun onReceive(context: Context?, intent: Intent?) {
         val intentAction = intent?.action ?: return
-        if(intentAction == Intent.ACTION_SCREEN_OFF || intentAction == Intent.ACTION_SCREEN_ON){
-            if(_count == 0) {
+        if (intentAction == Intent.ACTION_SCREEN_OFF || intentAction == Intent.ACTION_SCREEN_ON) {
+            if (_count == 0) {
                 _timer.run()
             }
-            if(_count == 5) {
-                runBlocking {
-                    saveMyLifeRepository.setIsDangerModeEnabled(true)
+            if (_count == 5) {
+                scope.launch {
+                    val messageSendingInterval = getMessageSendingIntervalFlowUseCase().first()
+                    val doInDangerRequest =
+                        PeriodicWorkRequestBuilder<DoInDangerWorker>(
+                            repeatInterval = messageSendingInterval.toJavaDuration()
+                        ).build()
+                    WorkManager.getInstance(applicationContext).enqueue(doInDangerRequest)
                 }
+//                scope.launch {
+//                    saveMyLifeRepository.setIsDangerModeEnabled(true)
+//                }
 //                Intent(context, MainService::class.java).also {
 //                    it.action = ActionConstants.SwitchDangerMode
 //                    context?.startService(it)
