00d0ef37965a86be6784c6ce103bc2e36f7df1e6
FreePhoenix888 <freephoenix888@gmail.com>
2022-06-27 11:32:40 +0600

    Add settings field

--- app/src/main/java/com/freephoenix888/savemylife/data/repositories/LocationRepository.kt
+++ app/src/main/java/com/freephoenix888/savemylife/data/repositories/LocationRepository.kt
@@ -1,12 +1,17 @@
 package com.freephoenix888.savemylife.data.repositories
 
 import com.freephoenix888.savemylife.data.sources.interfaces.LocationLocalDataSource
+import com.freephoenix888.savemylife.mappers.LocationPreferencesToLocationSettingsMapper
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class LocationRepository @Inject constructor(private val locationLocalDataSource: LocationLocalDataSource) {
-    val isLocationSharingEnabled = locationLocalDataSource.isLocationSharingEnabled
+class LocationRepository @Inject constructor(private val locationLocalDataSource: LocationLocalDataSource, private val locationPreferencesToLocationSettingsMapper: LocationPreferencesToLocationSettingsMapper) {
+    val settings = locationLocalDataSource.preferences.map {
+        locationPreferencesToLocationSettingsMapper.map(it)
+    }
+
     suspend fun setIsLocationSharingEnabled(newLocationSharingState: Boolean) {
         locationLocalDataSource.setIsLocationSharingEnabled(newLocationSharingState)
     }
