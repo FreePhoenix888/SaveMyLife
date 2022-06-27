63f6fef08d55b3b0453fde5d66860e85297a9ebd
FreePhoenix888 <freephoenix888@gmail.com>
2022-06-27 11:35:17 +0600

    Фвв preferences field

--- app/src/main/java/com/freephoenix888/savemylife/data/sources/LocationDataStoreLocalDataSource.kt
+++ app/src/main/java/com/freephoenix888/savemylife/data/sources/LocationDataStoreLocalDataSource.kt
@@ -4,16 +4,13 @@ import androidx.datastore.core.DataStore
 import com.freephoenix888.savemylife.LocationPreferences
 import com.freephoenix888.savemylife.data.sources.interfaces.LocationLocalDataSource
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 class LocationDataStoreLocalDataSource @Inject constructor(private val dataStore: DataStore<LocationPreferences>) :
     LocationLocalDataSource {
-    override val isLocationSharingEnabled: Flow<Boolean> = dataStore.data.map {
-        it.isLocationSharingEnabled
-    }
+    override val preferences: Flow<LocationPreferences> = dataStore.data
 
     override suspend fun setIsLocationSharingEnabled(newState: Boolean) {
         dataStore.updateData {
--- app/src/main/java/com/freephoenix888/savemylife/data/sources/MessageDataStoreLocalDataSource.kt
+++ app/src/main/java/com/freephoenix888/savemylife/data/sources/MessageDataStoreLocalDataSource.kt
@@ -2,37 +2,29 @@ package com.freephoenix888.savemylife.data.sources
 
 import androidx.datastore.core.DataStore
 import com.freephoenix888.savemylife.MessagePreferences
-import com.freephoenix888.savemylife.SecondsInterval
 import com.freephoenix888.savemylife.data.sources.interfaces.MessageLocalDataSource
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
+import kotlin.time.Duration
 
 @Singleton
 class MessageDataStoreLocalDataSource @Inject constructor(private val dataStore: DataStore<MessagePreferences>) :
     MessageLocalDataSource {
 
-    override val messageTemplate: Flow<String> = dataStore.data.map {
-        it.messageTemplate
-    }
+    override val preferences = dataStore.data
 
     override suspend fun setMessageTemplate(newMessageTemplate: String) {
         dataStore.updateData {
             it.toBuilder()
-                .setMessageTemplate(newMessageTemplate)
+                .setTemplate(newMessageTemplate)
                 .build()
         }
     }
 
-    override val sendingInterval: Flow<SecondsInterval> = dataStore.data.map {
-        it.sendingIntervalInSeconds
-    }
-
-    override suspend fun setSendingInterval(newSendingInterval: SecondsInterval) {
+    override suspend fun setSendingInterval(newSendingInterval: Duration) {
         dataStore.updateData {
             it.toBuilder()
-                .setSendingIntervalInSeconds(newSendingInterval)
+                .setSendingIntervalInSeconds(newSendingInterval.inWholeSeconds)
                 .build()
         }
     }
