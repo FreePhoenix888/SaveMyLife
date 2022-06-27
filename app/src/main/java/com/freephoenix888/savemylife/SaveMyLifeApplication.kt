81e2748d7859d6c47dae98c71a72186fca90209e
FreePhoenix888 <freephoenix888@gmail.com>
2022-06-27 14:32:15 +0600

    Inject worker factory and configure it

--- app/src/main/java/com/freephoenix888/savemylife/SaveMyLifeApplication.kt
+++ app/src/main/java/com/freephoenix888/savemylife/SaveMyLifeApplication.kt
@@ -1,9 +1,18 @@
 package com.freephoenix888.savemylife
 
 import android.app.Application
+import androidx.hilt.work.HiltWorkerFactory
+import androidx.work.Configuration
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class SaveMyLifeApplication() : Application() {
+class SaveMyLifeApplication() : Application(), Configuration.Provider {
+    @Inject
+    lateinit var workerFactory: HiltWorkerFactory
 
+    override fun getWorkManagerConfiguration() =
+        Configuration.Builder()
+            .setWorkerFactory(workerFactory)
+            .build()
 }
