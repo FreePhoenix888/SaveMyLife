1234d421620859e2ab16022e6324d560dd263a18
FreePhoenix888 <freephoenix888@gmail.com>
2022-07-03 16:36:22 +0600

    Make service workable

--- app/src/main/java/com/freephoenix888/savemylife/services/MainService.kt
+++ app/src/main/java/com/freephoenix888/savemylife/services/MainService.kt
@@ -1,10 +1,13 @@
 package com.freephoenix888.savemylife.services
 
-import android.app.*
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
-import android.icu.util.Calendar
+import android.graphics.BitmapFactory
 import android.os.Build
 import android.util.Log
 import androidx.annotation.RequiresApi
@@ -13,108 +16,101 @@ import androidx.core.app.TaskStackBuilder
 import androidx.core.net.toUri
 import androidx.lifecycle.LifecycleService
 import com.freephoenix888.savemylife.R
+import com.freephoenix888.savemylife.Utils
 import com.freephoenix888.savemylife.broadcastReceivers.PowerButtonBroadcastReceiver
 import com.freephoenix888.savemylife.broadcastReceivers.RestartBroadcastReceiver
 import com.freephoenix888.savemylife.constants.ActionConstants
 import com.freephoenix888.savemylife.constants.Constants
+import com.freephoenix888.savemylife.constants.MessageConstants
 import com.freephoenix888.savemylife.constants.NotificationConstants
+import com.freephoenix888.savemylife.domain.models.PhoneNumber
 import com.freephoenix888.savemylife.domain.useCases.GetIsDangerModeEnabledFlowUseCase
 import com.freephoenix888.savemylife.domain.useCases.GetMessageSendingIntervalFlowUseCase
 import com.freephoenix888.savemylife.domain.useCases.GetMessageUseCase
 import com.freephoenix888.savemylife.domain.useCases.GetPhoneNumberListFlowUseCase
 import com.freephoenix888.savemylife.ui.SaveMyLifeActivity
 import com.freephoenix888.savemylife.ui.SaveMyLifeScreenEnum
-import com.freephoenix888.savemylife.workers.DoInDangerBroadcastReceiver
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.*
-import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.MutableStateFlow
 import javax.inject.Inject
 
 
 @AndroidEntryPoint
-class MainService: LifecycleService() {
+class MainService : LifecycleService() {
 
-    val powerButtonBroadcastReceiver = PowerButtonBroadcastReceiver()
-    val doInDangerBroadcastReceiver = DoInDangerBroadcastReceiver()
-    @Inject lateinit var getIsDangerModeEnabledFlowUseCase: GetIsDangerModeEnabledFlowUseCase
-    @Inject lateinit var getPhoneNumberListFlowUseCase: GetPhoneNumberListFlowUseCase
-    @Inject lateinit var getMessageUseCase: GetMessageUseCase
-    @Inject lateinit var getMessageSendingIntervalFlowUseCase: GetMessageSendingIntervalFlowUseCase
+    @Inject
+    lateinit var getIsDangerModeEnabledFlowUseCase: GetIsDangerModeEnabledFlowUseCase
+    @Inject
+    lateinit var getPhoneNumberListFlowUseCase: GetPhoneNumberListFlowUseCase
+    @Inject
+    lateinit var getMessageUseCase: GetMessageUseCase
+    @Inject
+    lateinit var getMessageSendingIntervalFlowUseCase: GetMessageSendingIntervalFlowUseCase
 
-    var isFirstStart = true
-    private val TAG = this::class.simpleName
+    private var isFirstStart = true
     private val coroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
-    private lateinit var isDangerModeEnabled: Flow<Boolean>
-    private var doInDangerSituationJob: Job? = null
+    private var isDangerModeEnabled: MutableStateFlow<Boolean> = MutableStateFlow(false)
+    private var doInDangerJob: Job? = null
+    private val messageSendingInterval = MutableStateFlow(MessageConstants.DEFAULT_SENDING_INTERVAL)
+    private val phoneNumberList = MutableStateFlow(emptyList<PhoneNumber>())
+    private val powerButtonBroadcastReceiver = PowerButtonBroadcastReceiver()
+
 
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
         super.onStartCommand(intent, flags, startId)
-        isDangerModeEnabled = getIsDangerModeEnabledFlowUseCase()
-        coroutineScope.launch {
-            isDangerModeEnabled.collect {
-                val intent: Intent = Intent(applicationContext, DoInDangerBroadcastReceiver::class.java)
-                val flags = if(Build.VERSION.SDK_INT >= 31) PendingIntent.FLAG_IMMUTABLE else PendingIntent.FLAG_UPDATE_CURRENT
-                val pendingIntent = PendingIntent.getBroadcast(applicationContext, 0, intent, flags)
-                if(it){
-                    val messageSendingInterval = getMessageSendingIntervalFlowUseCase().first()
-                    val alarmManager = applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-                    alarmManager.setRepeating(
-                        AlarmManager.RTC,
-                        Calendar.getInstance().timeInMillis,
-                        messageSendingInterval.inWholeMilliseconds,
-                        pendingIntent
-                    )
-//                    alarmManager.setExactAndAllowWhileIdle(
-//                        AlarmManager.RTC,
-//                        messageSendingInterval.inWholeMilliseconds,
-//                        pendingIntent
-//                    )
-                } else {
-                    val alarmManager = applicationContext.getSystemService(Context.ALARM_SERVICE) as AlarmManager
-                    alarmManager.cancel(pendingIntent)
-                }
-//                val doInDangerRequest =
-//                    PeriodicWorkRequestBuilder<DoInDangerWorker>(
-//                        repeatInterval = messageSendingInterval.inWholeSeconds, repeatIntervalTimeUnit = TimeUnit.SECONDS
-//                    ).build()
-//                WorkManager.getInstance(applicationContext).enqueue(doInDangerRequest)
-
-            }
-
-        }
         if (intent == null) {
             return START_STICKY
         }
         when (intent.action) {
             ActionConstants.StartMainService -> {
                 if (isFirstStart) {
-                    Log.d(TAG, "First start...")
                     startForegroundService()
                     isFirstStart = false
                 }
             }
-            ActionConstants.StopMainService -> {
-                Log.d(TAG, "Stopping...")
-            }
             else -> {}
         }
+
+        registerPowerButtonBroadcastReceiver()
+
+        coroutineScope.launch {
+            getMessageSendingIntervalFlowUseCase().collect {
+                messageSendingInterval.value = it
+                Log.d(null, "new interval in service: ${messageSendingInterval.value}")
+            }
+        }
+        coroutineScope.launch {
+            getPhoneNumberListFlowUseCase().collect {
+                phoneNumberList.value = it
+            }
+        }
+        coroutineScope.launch {
+            getIsDangerModeEnabledFlowUseCase().collect {
+                isDangerModeEnabled.value = it
+                doInDangerJob?.cancel()
+                if (it) {
+                    doInDangerJob = doInDanger()
+                }
+            }
+        }
+
+        return START_STICKY
+    }
+
+    private fun registerPowerButtonBroadcastReceiver() {
         val filter = IntentFilter().apply {
             addAction(Intent.ACTION_SCREEN_ON)
             addAction(Intent.ACTION_SCREEN_OFF)
         }
-        this.registerReceiver(powerButtonBroadcastReceiver, filter)
-        this.registerReceiver(doInDangerBroadcastReceiver, filter)
-        return START_STICKY
+        registerReceiver(powerButtonBroadcastReceiver, filter)
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        Log.i(TAG, "$TAG is destroyed.")
         val broadcastIntent = Intent(this, RestartBroadcastReceiver::class.java)
         sendBroadcast(broadcastIntent)
         unregisterReceiver(powerButtonBroadcastReceiver)
-        unregisterReceiver(doInDangerBroadcastReceiver)
     }
 
     private fun startForegroundService() {
@@ -131,41 +127,27 @@ class MainService: LifecycleService() {
         val notificationBuilder = NotificationCompat.Builder(this, NotificationConstants.CHANNEL_ID)
             .setAutoCancel(false)
             .setOngoing(true)
-//            .setLargeIcon(BitmapFactory.decodeResource(resources, R.drawable.ic_baseline_warning_24))
-            .setSmallIcon(R.drawable.ic_baseline_warning_24)
+            .setSmallIcon(R.drawable.ic_stat_name)
+            .setLargeIcon(
+                BitmapFactory.decodeResource(applicationContext.resources, R.mipmap.ic_launcher))
             .setContentTitle("SaveMyLife")
             .setContentText("SaveMyLife is active")
-            .setContentIntent(getButtonActivityPendingIntent())
-
+            .setContentIntent(getDangerButtonActivityPendingIntent())
         return notificationBuilder.build()
     }
 
-    private fun getButtonActivityPendingIntent(): PendingIntent {
+    private fun getDangerButtonActivityPendingIntent(): PendingIntent {
         val dangerButtonIntent = Intent(
             Intent.ACTION_VIEW,
             "${Constants.APP_URI}/screen/${SaveMyLifeScreenEnum.DangerButton.name}".toUri(),
             applicationContext,
             SaveMyLifeActivity::class.java
         )
-
-
-//        val pendingIntent: PendingIntent
-//        val intent = Intent(this, SaveMyLifeActivity::class.java).also {
-//            it.action = ActionConstants.ShowButtonScreen
-//        }
-
-
         val flags: Int = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             PendingIntent.FLAG_IMMUTABLE or PendingIntent.FLAG_UPDATE_CURRENT
         } else {
             PendingIntent.FLAG_UPDATE_CURRENT
         }
-//        pendingIntent = PendingIntent.getActivity(
-//            this,
-//            0,
-//            intent,
-//            flags
-//        )
         val pendingIntent = TaskStackBuilder.create(applicationContext).run {
             addNextIntentWithParentStack(dangerButtonIntent)
             getPendingIntent(0, flags)
@@ -182,5 +164,19 @@ class MainService: LifecycleService() {
         )
         notificationManager.createNotificationChannel(channel)
     }
+
+    private fun doInDanger() = coroutineScope.launch {
+        while (true) {
+            for (phoneNumber in phoneNumberList.value) {
+                Utils.sendSms(
+                    context = applicationContext,
+                    phoneNumber = phoneNumber.phoneNumber,
+                    message = getMessageUseCase(phoneNumber)
+                )
+            }
+            delay(messageSendingInterval.value)
+        }
+    }
+
 
 }
