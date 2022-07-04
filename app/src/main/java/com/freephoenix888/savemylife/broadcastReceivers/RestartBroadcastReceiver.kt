e6d9ae8df6d26f43e317b9771c9c08206eb0dead
FreePhoenix888 <freephoenix888@gmail.com>
2022-07-04 13:53:46 +0600

    Log receiver

--- app/src/main/java/com/freephoenix888/savemylife/broadcastReceivers/RestartBroadcastReceiver.kt
+++ app/src/main/java/com/freephoenix888/savemylife/broadcastReceivers/RestartBroadcastReceiver.kt
@@ -3,12 +3,26 @@ package com.freephoenix888.savemylife.broadcastReceivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.util.Log
+import android.widget.Toast
 import com.freephoenix888.savemylife.services.MainService
 import dagger.hilt.android.AndroidEntryPoint
 
+
+private const val TAG = "RestartBroadcastReceiver"
 @AndroidEntryPoint
 class RestartBroadcastReceiver : BroadcastReceiver() {
-    override fun onReceive(context: Context?, intent: Intent?) {
-        context?.startService(Intent(context, MainService::class.java))
+
+    override fun onReceive(context: Context, intent: Intent) {
+        context.startService(Intent(context, MainService::class.java))
+        Log.d(null, "RestartBroadcastReceiver onReceive: ")
+        StringBuilder().apply {
+            append("RestartBroadcastReceiver Action: ${intent.action}\n")
+            append("RestartBroadcastReceiver URI: ${intent.toUri(Intent.URI_INTENT_SCHEME)}\n")
+            toString().also { log ->
+                Log.d(null, log)
+                Toast.makeText(context, log, Toast.LENGTH_LONG).show()
+            }
+        }
     }
 }
