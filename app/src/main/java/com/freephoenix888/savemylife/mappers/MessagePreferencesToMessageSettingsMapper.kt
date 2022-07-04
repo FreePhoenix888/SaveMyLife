5c10cb5f7749177a8e0f4250dfd88bf84e2e083c
FreePhoenix888 <freephoenix888@gmail.com>
2022-07-04 14:12:11 +0600

    Update mapper to use default values from constants instead of data store default values

--- app/src/main/java/com/freephoenix888/savemylife/mappers/MessagePreferencesToMessageSettingsMapper.kt
+++ app/src/main/java/com/freephoenix888/savemylife/mappers/MessagePreferencesToMessageSettingsMapper.kt
@@ -1,6 +1,7 @@
 package com.freephoenix888.savemylife.mappers
 
 import com.freephoenix888.savemylife.MessagePreferences
+import com.freephoenix888.savemylife.constants.MessageConstants
 import com.freephoenix888.savemylife.domain.models.MessageSettings
 import javax.inject.Inject
 import kotlin.time.DurationUnit
@@ -9,8 +10,8 @@ import kotlin.time.toDuration
 class MessagePreferencesToMessageSettingsMapper @Inject constructor(): Mapper<MessagePreferences, MessageSettings> {
     override fun map(input: MessagePreferences): MessageSettings {
         return MessageSettings(
-            template = input.template,
-            sendingInterval = input.sendingIntervalInSeconds.toDuration(DurationUnit.SECONDS)
+            template = input.template.ifEmpty { MessageConstants.DEFAULT_TEMPLATE },
+            sendingInterval = if (input.sendingIntervalInSeconds == 0L) MessageConstants.DEFAULT_SENDING_INTERVAL else input.sendingIntervalInSeconds.toDuration(DurationUnit.SECONDS)
         )
     }
 }
