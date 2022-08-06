3ae455c07749e4f10e70a45aca1c7701cb773c2b
FreePhoenix888 <freephoenix888@gmail.com>
2022-08-06 11:44:50 +0600

    Use object instead of class with companion object

--- app/src/main/java/com/freephoenix888/savemylife/Utils.kt
+++ app/src/main/java/com/freephoenix888/savemylife/Utils.kt
@@ -13,18 +13,18 @@ import androidx.core.net.toUri
 import com.freephoenix888.savemylife.domain.models.PhoneNumber
 
 
-class Utils {
-    companion object {
-        fun openAppSettings(context: Context) {
-            val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
-            val uri = Uri.fromParts("package", context.packageName, null)
-            intent.data = uri
-            ContextCompat.startActivity(context, intent, null)
-        }
+object Utils {
 
-        fun getContactLookupUri(id: Long, lookupKey: String): Uri {
-            return ContactsContract.Contacts.getLookupUri(id, lookupKey)
-        }
+    fun openAppSettings(context: Context) {
+        val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+        val uri = Uri.fromParts("package", context.packageName, null)
+        intent.data = uri
+        ContextCompat.startActivity(context, intent, null)
+    }
+
+    fun getContactLookupUri(id: Long, lookupKey: String): Uri {
+        return ContactsContract.Contacts.getLookupUri(id, lookupKey)
+    }
 
 //        fun getContactByLookupUri(context: Context, lookupUri: Uri): Contact {
 //            val contestResolver = context.contentResolver
@@ -51,42 +51,56 @@ class Utils {
 //                )
 //            } else throw Throwable("Contact with lookup uri $lookupUri does not exist.")
 //        }
-        
-        fun getPhoneNumberByContentUri(context: Context, contentUri: Uri): PhoneNumber {
-            val contestResolver = context.contentResolver
-            val cursor = contestResolver.query(contentUri, arrayOf(ContactsContract.CommonDataKinds.Phone.NUMBER, ContactsContract.CommonDataKinds.Phone.PHOTO_THUMBNAIL_URI, ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME), null, null, null)
-                ?: throw Throwable("Phone number with content uri $contentUri does not exist.")
-            if (cursor.moveToFirst()) {
-                val phoneNumberColumnIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
-                val phoneNumber = cursor.getString(phoneNumberColumnIndex)
 
-                val contactPhotoThumbnailUriColumnIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_THUMBNAIL_URI)
-                val contactPhotoThumbnailUri = cursor.getString(contactPhotoThumbnailUriColumnIndex)?.toUri()
+    fun getPhoneNumberByContentUri(context: Context, contentUri: Uri): PhoneNumber {
+        val contestResolver = context.contentResolver
+        val cursor = contestResolver.query(
+            contentUri,
+            arrayOf(
+                ContactsContract.CommonDataKinds.Phone.NUMBER,
+                ContactsContract.CommonDataKinds.Phone.PHOTO_THUMBNAIL_URI,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME
+            ),
+            null,
+            null,
+            null
+        )
+            ?: throw Throwable("Phone number with content uri $contentUri does not exist.")
+        if (cursor.moveToFirst()) {
+            val phoneNumberColumnIndex =
+                cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER)
+            val phoneNumber = cursor.getString(phoneNumberColumnIndex)
 
-                val contactDisplayNameColumnIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
-                val contactDisplayName = cursor.getString(contactDisplayNameColumnIndex)
+            val contactPhotoThumbnailUriColumnIndex =
+                cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.PHOTO_THUMBNAIL_URI)
+            val contactPhotoThumbnailUri =
+                cursor.getString(contactPhotoThumbnailUriColumnIndex)?.toUri()
 
-                cursor.close()
-                return PhoneNumber(
-                    phoneNumber = phoneNumber,
-                    contactName = contactDisplayName,
-                    contactImageThumbnailUri = contactPhotoThumbnailUri,
-                    contentUri = contentUri
-                )
-            } else throw Throwable("Phone number with content uri $contentUri does not exist.")
-        }
+            val contactDisplayNameColumnIndex =
+                cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME)
+            val contactDisplayName = cursor.getString(contactDisplayNameColumnIndex)
 
-        fun sendSms(context: Context, phoneNumber: String, message: String) {
-            val flags = if(Build.VERSION.SDK_INT >= 23) PendingIntent.FLAG_IMMUTABLE else 0
-            val sentPI: PendingIntent = PendingIntent.getBroadcast(context, 0, Intent("SMS_SENT"), flags)
-            @Suppress("DEPRECATION") val smsManager: SmsManager =
-                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                    context.getSystemService(SmsManager::class.java)
-                } else {
-                    SmsManager.getDefault()
-                }
-            smsManager.sendTextMessage(phoneNumber, null, message, sentPI, null)
-        }
-
+            cursor.close()
+            return PhoneNumber(
+                phoneNumber = phoneNumber,
+                contactName = contactDisplayName,
+                contactImageThumbnailUri = contactPhotoThumbnailUri,
+                contentUri = contentUri
+            )
+        } else throw Throwable("Phone number with content uri $contentUri does not exist.")
     }
+
+    fun sendSms(context: Context, phoneNumber: String, message: String) {
+        val flags = if (Build.VERSION.SDK_INT >= 23) PendingIntent.FLAG_IMMUTABLE else 0
+        val sentPI: PendingIntent =
+            PendingIntent.getBroadcast(context, 0, Intent("SMS_SENT"), flags)
+        @Suppress("DEPRECATION") val smsManager: SmsManager =
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                context.getSystemService(SmsManager::class.java)
+            } else {
+                SmsManager.getDefault()
+            }
+        smsManager.sendTextMessage(phoneNumber, null, message, sentPI, null)
+    }
+
 }
