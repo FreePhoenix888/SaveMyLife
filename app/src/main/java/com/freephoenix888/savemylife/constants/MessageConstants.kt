a2a1488d9f320f78c993ce1bde8a0c66d7f68ffa
FreePhoenix888 <freephoenix888@gmail.com>
2022-08-06 11:45:02 +0600

    And MessageTemplateVariables enum class

--- app/src/main/java/com/freephoenix888/savemylife/constants/MessageConstants.kt
+++ app/src/main/java/com/freephoenix888/savemylife/constants/MessageConstants.kt
@@ -4,6 +4,11 @@ import kotlin.time.Duration
 import kotlin.time.DurationUnit
 import kotlin.time.toDuration
 
+enum class MessageTemplateVariables {
+    CONTACT_NAME,
+    LOCATION_URL
+}
+
 object MessageConstants {
     val DEFAULT_SENDING_INTERVAL: Duration =  (30L).toDuration(DurationUnit.MINUTES)
     const val DEFAULT_TEMPLATE: String = "{CONTACT_NAME}, I AM IN DANGER! My location: {LOCATION_URL}"
