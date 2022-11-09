74eb83bbd1df188b582a9603e8416941f8c4fc66
FreePhoenix888 <freephoenix888@gmail.com>
2022-11-09 20:08:05 +0600

    Replace message commands too (draft)

--- app/src/main/java/com/freephoenix888/savemylife/domain/useCases/GetMessageUseCase.kt
+++ app/src/main/java/com/freephoenix888/savemylife/domain/useCases/GetMessageUseCase.kt
@@ -1,5 +1,7 @@
 package com.freephoenix888.savemylife.domain.useCases
 
+import android.util.Log
+import com.freephoenix888.savemylife.constants.MessageTemplateVariables
 import com.freephoenix888.savemylife.domain.models.PhoneNumber
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
@@ -10,7 +12,11 @@ class GetMessageUseCase @Inject constructor(
 ) {
     suspend operator fun invoke(phoneNumber: PhoneNumber): String {
         val message = getMessageTemplateFlowUseCase().first()
-        return message.replace("{CONTACT_NAME}", phoneNumber.contactName)
-            .replace("{LOCATION_URL}", getUserLocationUrlUseCase())
+        return message.replace("{${MessageTemplateVariables.CONTACT_NAME.name}}", phoneNumber.contactName)
+            .replace("{${MessageTemplateVariables.LOCATION_URL.name}}", getUserLocationUrlUseCase())
+            .replace(regex = Regex.fromLiteral("{${MessageTemplateVariables.MESSAGE_COMMANDS.name}}")) { matchResult ->
+                Log.d(null, "GetMessageUseCase invoke: ${matchResult}")
+                return@replace matchResult.value
+            }
     }
 }
