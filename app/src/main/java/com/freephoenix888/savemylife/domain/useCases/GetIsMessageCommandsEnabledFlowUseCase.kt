84f910852faf3a7c606859d89e605bf777c194ca
FreePhoenix888 <freephoenix888@gmail.com>
2022-11-18 15:49:05 +0600

    Return flow

--- app/src/main/java/com/freephoenix888/savemylife/domain/useCases/GetIsMessageCommandsEnabledFlowUseCase.kt
+++ app/src/main/java/com/freephoenix888/savemylife/domain/useCases/GetIsMessageCommandsEnabledFlowUseCase.kt
@@ -1,11 +1,12 @@
 package com.freephoenix888.savemylife.domain.useCases
 
 import com.freephoenix888.savemylife.data.repositories.MessageRepository
+import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 
 class GetIsMessageCommandsEnabledFlowUseCase @Inject constructor(private val messageRepository: MessageRepository) {
-    suspend operator fun invoke() {
-        messageRepository.settings.map { it.isMessageCommandsEnabled }
+    suspend operator fun invoke(): Flow<Boolean> {
+        return messageRepository.settings.map { it.isMessageCommandsEnabled }
     }
 }
