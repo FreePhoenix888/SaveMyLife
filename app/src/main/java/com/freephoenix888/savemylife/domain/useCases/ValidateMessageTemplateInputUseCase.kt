95bc50cfe90e537ba85f0c420136c60e92ef1b43
FreePhoenix888 <freephoenix888@gmail.com>
2022-11-18 15:49:36 +0600

    Check if MESSAGE_COMMANDS command is located and behave accordingly

--- app/src/main/java/com/freephoenix888/savemylife/domain/useCases/ValidateMessageTemplateInputUseCase.kt
+++ app/src/main/java/com/freephoenix888/savemylife/domain/useCases/ValidateMessageTemplateInputUseCase.kt
@@ -5,12 +5,14 @@ import com.freephoenix888.savemylife.domain.models.ValidationResult
 import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 
-class ValidateMessageTemplateInputUseCase @Inject constructor(val getIsLocationSharingEnabledFlowUseCase: GetIsLocationSharingEnabledFlowUseCase) {
+class ValidateMessageTemplateInputUseCase @Inject constructor(val getIsLocationSharingEnabledFlowUseCase: GetIsLocationSharingEnabledFlowUseCase, val getIsMessageCommandsEnabledFlowUseCase: GetIsMessageCommandsEnabledFlowUseCase) {
     suspend operator fun invoke(input: String): ValidationResult {
         return if (input.isEmpty()) {
             ValidationResult.Error("Message template must not be empty")
         } else if (input.contains("{${MessageTemplateVariables.LOCATION_URL.name}}") && !getIsLocationSharingEnabledFlowUseCase().first()){
             ValidationResult.Error("Message template cannot contain {LOCATION_URL} if location sharing is disabled")
+        }else if (input.contains("{${MessageTemplateVariables.MESSAGE_COMMANDS.name}}") && !getIsMessageCommandsEnabledFlowUseCase().first()){
+            ValidationResult.Error("Message template cannot contain {MESSAGE_COMMANDS} if message commands feature is disabled")
         }
         else {
             ValidationResult.Success
