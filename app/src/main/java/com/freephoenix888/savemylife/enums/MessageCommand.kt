f6dca357cdcf991256997797c558249d1bc1c1d2
FreePhoenix888 <freephoenix888@gmail.com>
2022-11-19 18:03:41 +0600

    Use Enum with description val to describe MessageCommands

--- app/src/main/java/com/freephoenix888/savemylife/enums/MessageCommand.kt
+++ app/src/main/java/com/freephoenix888/savemylife/enums/MessageCommand.kt
@@ -1,19 +1,5 @@
 package com.freephoenix888.savemylife.enums
 
-enum class MessageCommand {
-    LOCATION
-}
-
-val messageCommandDescriptionMap = mapOf(
-    MessageCommand.LOCATION to "Current location",
-)
-
-val messageCommandsAndDescriptionsString = messageCommandDescriptionMap.entries.joinToString { entry: Map.Entry<MessageCommand, String> ->
-    "/${entry.key.name.lowercase()} - ${entry.value}"
-}
-
-fun getMessageCommandDescription(messageCommand: MessageCommand): String {
-    return when (messageCommand) {
-        MessageCommand.LOCATION -> "Current location"
-    }
+enum class MessageCommand(val description: String) {
+    LOCATION("Get current location")
 }
