c4fa876e40415477ee70e88c6e48155d6da152c2
FreePhoenix888 <freephoenix888@gmail.com>
2022-11-19 18:11:40 +0600

    Add ACTION_LOCKED_BOOT_COMPLETED to intent.action condition. Use startForeGroundService on SDK_INT >= 26

--- app/src/main/java/com/freephoenix888/savemylife/broadcastReceivers/RestartBroadcastReceiver.kt
+++ app/src/main/java/com/freephoenix888/savemylife/broadcastReceivers/RestartBroadcastReceiver.kt
@@ -3,6 +3,7 @@ package com.freephoenix888.savemylife.broadcastReceivers
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import android.os.Build
 import com.freephoenix888.savemylife.services.MainService
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,8 +13,15 @@ private const val TAG = "RestartBroadcastReceiver"
 class RestartBroadcastReceiver : BroadcastReceiver() {
 
     override fun onReceive(context: Context, intent: Intent) {
-       if(intent.action == Intent.ACTION_BOOT_COMPLETED) {
-           context.startService(Intent(context, MainService::class.java))
+       if(intent.action == Intent.ACTION_BOOT_COMPLETED || intent.action == Intent.ACTION_LOCKED_BOOT_COMPLETED) {
+           Intent(context, MainService::class.java).also {
+               if(Build.VERSION.SDK_INT >= 26) {
+                   context.startForegroundService(it)
+               } else {
+                   context.startService(it)
+               }
+           }
+
        }
     }
 }
