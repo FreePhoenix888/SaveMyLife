839e2a98a3b06a2fefe1a11d4875d70604c1b4f9
FreePhoenix888 <freephoenix888@gmail.com>
2022-11-20 15:15:23 +0600

    Add navController to parameters

--- app/src/main/java/com/freephoenix888/savemylife/ui/composables/SaveMyLifeApp.kt
+++ app/src/main/java/com/freephoenix888/savemylife/ui/composables/SaveMyLifeApp.kt
@@ -2,14 +2,16 @@ package com.freephoenix888.savemylife.ui.composables
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.freephoenix888.savemylife.navigation.navGraph.SaveMyLifeNavGraph
 import com.freephoenix888.savemylife.ui.theme.SaveMyLifeTheme
 
 @Composable
-fun SaveMyLifeApp() {
+fun SaveMyLifeApp(navController: NavHostController) {
     SaveMyLifeTheme {
-        SaveMyLifeNavGraph(navController = rememberNavController())
+        SaveMyLifeNavGraph(navController = navController)
     }
 }
 
@@ -69,8 +71,8 @@ fun SaveMyLifeApp() {
 //    }
 //}
 
-@Preview
-@Composable
-private fun SaveMyLifeAppPreview() {
-    SaveMyLifeApp()
-}
+//@Preview
+//@Composable
+//private fun SaveMyLifeAppPreview() {
+//    SaveMyLifeApp()
+//}
