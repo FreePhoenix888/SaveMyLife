857a08c817d69450f9b6237b5774ca198b5bd016
FreePhoenix888 <freephoenix888@gmail.com>
2022-11-20 15:16:10 +0600

    Do not set alarm mode. startActivity with specific intent to tell that Danger mode must be enabled

--- app/src/main/java/com/freephoenix888/savemylife/broadcastReceivers/PowerButtonBroadcastReceiver.kt
+++ app/src/main/java/com/freephoenix888/savemylife/broadcastReceivers/PowerButtonBroadcastReceiver.kt
@@ -5,6 +5,8 @@ import android.content.Context
 import android.content.Intent
 import android.util.Log
 import com.freephoenix888.savemylife.domain.useCases.SetIsAlarmModeEnabledUseCase
+import com.freephoenix888.savemylife.enums.IntentAction
+import com.freephoenix888.savemylife.ui.SaveMyLifeActivity
 import dagger.hilt.android.AndroidEntryPoint
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.CoroutineScope
@@ -32,7 +34,7 @@ class PowerButtonBroadcastReceiver :
 
     private var _count = 0
 
-    override fun onReceive(context: Context?, intent: Intent?) {
+    override fun onReceive(context: Context, intent: Intent?) {
         val intentAction = intent?.action ?: return
         val isScreenOnOrOff = intentAction == Intent.ACTION_SCREEN_OFF || intentAction == Intent.ACTION_SCREEN_ON
         if (!isScreenOnOrOff) {
@@ -45,7 +47,12 @@ class PowerButtonBroadcastReceiver :
         }
         if (_count == 5) {
             scope.launch {
-                setIsAlarmModeEnabledUseCase(true)
+//                setIsAlarmModeEnabledUseCase(true)
+                val saveMyLifeActivityIntent = Intent(context, SaveMyLifeActivity::class.java)
+                saveMyLifeActivityIntent.action = IntentAction.EnableDangerMode.name
+                saveMyLifeActivityIntent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+                saveMyLifeActivityIntent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+                context.startActivity(saveMyLifeActivityIntent)
             }
         }
         _count++
