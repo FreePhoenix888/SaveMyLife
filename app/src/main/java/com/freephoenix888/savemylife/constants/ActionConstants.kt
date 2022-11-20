98e775c7233b545b40b5eeaf9e8bccb95baf1bc3
FreePhoenix888 <freephoenix888@gmail.com>
2022-11-20 15:13:29 +0600

    Remove unused code

--- app/src/main/java/com/freephoenix888/savemylife/constants/ActionConstants.kt
+++
@@ -1,6 +0,0 @@
-package com.freephoenix888.savemylife.constants
-
-object ActionConstants {
-    const val StartMainService = "ACTION_START_SERVICE"
-    const val StopMainService = "ACTION_STOP_SERVICE"
-}
