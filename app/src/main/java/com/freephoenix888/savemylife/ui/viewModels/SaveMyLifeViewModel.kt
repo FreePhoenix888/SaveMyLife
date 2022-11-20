ba94ba6540aba0cb86d977ee6e22758e4b6cbd8b
FreePhoenix888 <freephoenix888@gmail.com>
2022-11-20 16:15:58 +0600

    Add openDangerModeActivationConfirmationScreenUseCase

--- app/src/main/java/com/freephoenix888/savemylife/ui/viewModels/SaveMyLifeViewModel.kt
+++ app/src/main/java/com/freephoenix888/savemylife/ui/viewModels/SaveMyLifeViewModel.kt
@@ -1,21 +1,25 @@
 package com.freephoenix888.savemylife.ui.viewModels
 
+import android.content.Context
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.freephoenix888.savemylife.domain.useCases.*
 import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class   SaveMyLifeViewModel @Inject constructor(
+class SaveMyLifeViewModel @Inject constructor(
+    @ApplicationContext val applicationContext: Context,
     getIsMainServiceEnabledFlowUseCase: GetIsMainServiceEnabledFlowUseCase,
     val switchIsMainServiceEnabledUseCase: SwitchIsMainServiceEnabledUseCase,
     getIsDangerModeEnabledFlowUseCase: GetIsDangerModeEnabledFlowUseCase,
     val switchIsDangerModeEnabledUseCase: SwitchIsDangerModeEnabledUseCase,
     val setIsDangerModeEnabledUseCase: SetIsDangerModeEnabledUseCase,
     getIsFirstAppLaunch: GetIsFirstAppLaunchFlowUseCase,
+    val openDangerModeActivationConfirmationScreenUseCase: OpenDangerModeActivationConfirmationScreenUseCase
 ) : ViewModel() {
     val isFirstAppLaunch = getIsFirstAppLaunch()
     val isMainServiceEnabled = getIsMainServiceEnabledFlowUseCase()
@@ -29,5 +33,9 @@ class   SaveMyLifeViewModel @Inject constructor(
     }
     fun setIsDangerModeEnabled(newValue: Boolean) = viewModelScope.launch(Dispatchers.IO) {
         setIsDangerModeEnabledUseCase(newValue)
+    }
+
+    fun openDangerModeActivationConfirmationScreenUseCase() {
+        openDangerModeActivationConfirmationScreenUseCase(applicationContext)
     }
 }
