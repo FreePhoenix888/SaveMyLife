d45ceeefcbd1c14541788833c963d87dd2bf2f6b
FreePhoenix888 <freephoenix888@gmail.com>
2022-12-14 16:20:09 +0600

    Remove default values

--- app/src/main/java/com/freephoenix888/savemylife/domain/models/LocationSettings.kt
+++ app/src/main/java/com/freephoenix888/savemylife/domain/models/LocationSettings.kt
@@ -1,5 +1,7 @@
 package com.freephoenix888.savemylife.domain.models
 
+import com.freephoenix888.savemylife.BatterySaverModeEnum.BatterySaverMode
+
 data class LocationSettings (
-    val isLocationSharingEnabled: Boolean = false
+    val isLocationSharingEnabled: Boolean,
         )
--- app/src/main/java/com/freephoenix888/savemylife/domain/models/MessageSettings.kt
+++ app/src/main/java/com/freephoenix888/savemylife/domain/models/MessageSettings.kt
@@ -1,9 +1,10 @@
 package com.freephoenix888.savemylife.domain.models
 
+import com.freephoenix888.savemylife.BatterySaverModeEnum.BatterySaverMode
 import kotlin.time.Duration
 
 data class MessageSettings(
     val template: String,
     val sendingInterval: Duration,
-    val isMessageCommandsEnabled: Boolean
+    val isMessageCommandsEnabled: Boolean,
 )
