e2d90a4218519cb614a413c86be33b01a9d090ef
FreePhoenix888 <freephoenix888@gmail.com>
2022-12-14 16:19:52 +0600

    Remove unused code

--- app/src/main/java/com/freephoenix888/savemylife/constants/Constants.kt
+++
@@ -1,6 +0,0 @@
-package com.freephoenix888.savemylife.constants
-
-object Constants {
-    const val APP_PACKAGE_NAME = "com.freephoenix888.savemylife"
-    const val APP_URI = "freephoenix888.savemylife://"
-}
