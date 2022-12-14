5b1ab684b07d9506d476382602438a4b28ac8008
FreePhoenix888 <freephoenix888@gmail.com>
2022-12-14 17:36:20 +0600

    Start main service on boot only if it must be launched

--- app/src/main/java/com/freephoenix888/savemylife/broadcastReceivers/BootCompletedBroadcastReceiver.kt
+++ app/src/main/java/com/freephoenix888/savemylife/broadcastReceivers/BootCompletedBroadcastReceiver.kt
@@ -4,34 +4,38 @@ import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.os.Build
-import android.os.Environment
-import android.util.Log
+import com.freephoenix888.savemylife.domain.useCases.GetIsMainServiceEnabledFlowUseCase
 import com.freephoenix888.savemylife.services.MainService
 import dagger.hilt.android.AndroidEntryPoint
-import java.io.File
-import java.io.FileOutputStream
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.launch
+import javax.inject.Inject
 
 
 private const val TAG = "RestartBroadcastReceiver"
 @AndroidEntryPoint
 class BootCompletedBroadcastReceiver : BroadcastReceiver() {
 
-    override fun onReceive(context: Context, intent: Intent) {
-        val file = File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS), "onboot.txt")
-        FileOutputStream(file).use {
-            it.write("SAVEMYLIFE onReceive: ${context}  ${intent}".toByteArray())
-        }
-//        val path = context.getExternalFilesDir(null)
-//        val
-       if(intent.action == Intent.ACTION_BOOT_COMPLETED || intent.action == Intent.ACTION_LOCKED_BOOT_COMPLETED) {
-           Intent(context, MainService::class.java).also {
-               if(Build.VERSION.SDK_INT >= 26) {
-                   context.startForegroundService(it)
-               } else {
-                   context.startService(it)
-               }
-           }
+    @Inject
+    lateinit var getIsMainServiceEnabledFlowUseCase: GetIsMainServiceEnabledFlowUseCase
 
-       }
+    private val coroutineScope = CoroutineScope(Dispatchers.IO + SupervisorJob())
+
+
+    override fun onReceive(context: Context, intent: Intent) {
+        coroutineScope.launch {
+            if (intent.action != Intent.ACTION_BOOT_COMPLETED && intent.action != Intent.ACTION_LOCKED_BOOT_COMPLETED) return@launch
+            if (!getIsMainServiceEnabledFlowUseCase().first()) return@launch
+            Intent(context, MainService::class.java).also {
+                if(Build.VERSION.SDK_INT >= 26) {
+                    context.startForegroundService(it)
+                } else {
+                    context.startService(it)
+                }
+            }
+        }
     }
 }
