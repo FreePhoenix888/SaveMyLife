51b685ec273aaa7710d03cd2de3ba688c7af336f
FreePhoenix888 <freephoenix888@gmail.com>
2022-12-14 16:26:29 +0600

    Make code more readable

--- app/src/main/java/com/freephoenix888/savemylife/domain/useCases/GetUserLocationUrlUseCase.kt
+++ app/src/main/java/com/freephoenix888/savemylife/domain/useCases/GetUserLocationUrlUseCase.kt
@@ -3,42 +3,42 @@ package com.freephoenix888.savemylife.domain.useCases
 import android.Manifest
 import android.content.Context
 import android.content.pm.PackageManager
+import android.os.PowerManager
 import androidx.core.content.ContextCompat
+import com.freephoenix888.savemylife.BatterySaverModeEnum
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
+import com.google.android.gms.location.LocationResult
 import com.google.android.gms.tasks.CancellationTokenSource
+import com.google.android.gms.tasks.Task
 import com.google.android.gms.tasks.Tasks
 import dagger.hilt.android.qualifiers.ApplicationContext
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class GetUserLocationUrlUseCase @Inject constructor(
     @ApplicationContext private val context: Context,
-    private val fusedLocationProviderClient: FusedLocationProviderClient
+    private val fusedLocationProviderClient: FusedLocationProviderClient,
 ) {
     suspend operator fun invoke(): String {
         if (ContextCompat.checkSelfPermission(
-                context,
-                Manifest.permission.ACCESS_FINE_LOCATION
+                context, Manifest.permission.ACCESS_FINE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED && ContextCompat.checkSelfPermission(
-                context,
-                Manifest.permission.ACCESS_COARSE_LOCATION
+                context, Manifest.permission.ACCESS_COARSE_LOCATION
             ) != PackageManager.PERMISSION_GRANTED
         ) {
             throw Throwable("Location permission not granted")
         }
-        val locationTask = fusedLocationProviderClient.getCurrentLocation(
-            LocationRequest.PRIORITY_HIGH_ACCURACY,
-            CancellationTokenSource().token
-        )
-        val locationUrl: String
-        withContext(Dispatchers.IO) {
-            Tasks.await(locationTask)
-            val location = locationTask.result
-            locationUrl = "https://maps.google.com/?q=${location.latitude},${location.longitude}"
+        val location = withContext(Dispatchers.IO) {
+            val getLocationTask =
+                fusedLocationProviderClient.getCurrentLocation(
+                    LocationRequest.PRIORITY_HIGH_ACCURACY, CancellationTokenSource().token
+                )
+            return@withContext Tasks.await(getLocationTask)
         }
-        return locationUrl
+        return "https://maps.google.com/?q=${location.latitude},${location.longitude}"
     }
 
 }
